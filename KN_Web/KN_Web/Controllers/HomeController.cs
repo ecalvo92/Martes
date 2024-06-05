05ef2de2afff55fafda9e2b22df6f3d489b34744
ecalvo92 <edcalvo92@hotmail.com>
2024-06-04 19:40:20 -0600

    Semana 3 - parte 1

--- KN_Web/KN_Web/Controllers/HomeController.cs
+++ KN_Web/KN_Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KN_Web.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,34 @@ namespace KN_Web.Controllers
 {
     public class HomeController : Controller
     {
-        /// <summary>
-        /// Nos redirecciona al inicio de sesión
-        /// </summary>
-        /// <returns></returns>
+        //Sirve para abrir las vistas
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        //Sirve para ejecutar las acciones dentro de una vista
+        [HttpPost]
+        public ActionResult Index(Usuario entidad)
+        {
+            //Validar los credenciales para determinar si lo dejamos continuar o lo rechazamos
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registro(Usuario entidad)
+        {
+            return View();
+        }
+
 
         public ActionResult Home()
         {
---
+++ KN_Web/KN_Web/Entidades/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace KN_Web.Entidades
+{
+    // Entidad - Objeto
+    public class Usuario
+    {
+        //Atributos - Propiedades
+        public string Correo { get; set; }
+        public string Contrasenna { get; set; }
+        public string Identificacion { get; set; }
+        public string Nombre { get; set; }
+    }
+}
